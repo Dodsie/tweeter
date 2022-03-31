b5dd444be6c965875e13d12116fffa1954ca95a6
Dodsie <baileydods@gmail.com>
2022-03-30 23:24:11 -0600

    added XSS prevevnt with escaping, added Validation errors (tweet field empty/over 1410 char max)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
 // Generates DOM structure with interpolated values from JSON/Objects.
 const createTweetElement = (tweetObject) => {
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  const safeHTML = `<p class="tweet-body" >${escape(tweetObject.content.text)}</p>`;
   const $tweet = (`
   <article class="tweet-container">
     <header class="tweets-header">
@@ -15,7 +22,7 @@ const createTweetElement = (tweetObject) => {
       </div>
       <p class="tweet-handle">${tweetObject.user.handle}</p>
     </header>
-    <p class="tweet-body">${tweetObject.content.text}</p>
+      ${safeHTML}
     <footer class="tweets-footer">
       <p>${timeago.format(tweetObject.created_at)}</p>
     <div class="tweets-actions">
@@ -41,6 +48,7 @@ const loadTweets = () => {
     url: "http://localhost:8080/tweets",
     type: "GET"
   }).then(function(data) {
+    $('#main-container').empty();
     renderTweets(data);
   });
 };
@@ -54,13 +62,26 @@ $(() => {
 // Listener, takes action on submit event.
   $('.tweet-form').on('submit', function(event) {
     event.preventDefault();
+    const $tweetText =  $('#tweet-text').val();
+    if ($tweetText === "") {
+      console.log('inside',$tweetText);
+      $(".error-message").text("Failed to Post - Tweet field must not be empty #EMPTYTWEET.");
+      $(".error-message").slideDown();
+      return;
+    }
+    if ($tweetText.length > 140) {
+      $(".error-message").text("Failed to Post - Tweet field has too many characters #WOAH.");
+      $(".error-message").slideDown();
+      return;
+    }
     $.ajax({
       url: "http://localhost:8080/tweets",
       type: 'POST',
       data: $('.tweet-form').serialize()
     }).then(() => {
-      $('#main-container').empty();
+      $(".error-message").slideUp();
       loadTweets();
+      $('form').trigger('reset');
     });
   });
 });
